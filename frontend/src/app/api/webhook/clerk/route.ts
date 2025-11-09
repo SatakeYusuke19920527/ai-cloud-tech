6c9943473ccddeab29d85a2b8dedb251de067595
SatakeYusuke19920527 <satkyusk@icloud.com>
2025-11-09 16:21:36 +0900

    fix: update error message for missing SIGNING_SECRET to specify .env.local

--- frontend/src/app/api/webhook/clerk/route.ts
+++ frontend/src/app/api/webhook/clerk/route.ts
@@ -8,7 +8,7 @@ export async function POST(req: Request) {
 
   if (!SIGNING_SECRET) {
     throw new Error(
-      'Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env'
+      'Error: Please add SIGNING_SECRET from Clerk Dashboard to .env.local'
     );
   }
 
